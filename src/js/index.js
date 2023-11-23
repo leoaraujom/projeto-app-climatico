6b12678bbe41eaf2dcf61c57f91f40be70bb54a1
Leonardo Maguerditchian de Araujo <56097536+leoaraujom@users.noreply.github.com>
2023-11-23 09:49:48 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,7 @@ botaoDeBusca.addEventListener("click", async () => {
 })
 
 async function buscarDadosDaCidade(cidade) {
-    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}aqi=no&lang=pt`;
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no`;
 
     const resposta = await fetch(apiUrl);
 
